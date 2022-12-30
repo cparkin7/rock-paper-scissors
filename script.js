887ac6baca9ac337cdd93a49cd623dacda5002a0
Chris Parkin <cparkin0828@gmail.com>
2022-12-30 15:42:47 -0500

    Add game function and isPoint function

--- script.js
+++ script.js
@@ -13,38 +13,81 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+function playRound() {
+  computerSelection = getComputerChoice();
+  playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+  let result;
+  console.log("You choose " + playerSelection);  // SEE PLAYER SELECTION
 
   // Conditions for rock
   if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats Rock";
+    result = "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats Scissors";
+    result = "You win! Rock beats Scissors";
   } else if (playerSelection === "rock" && computerSelection === "rock") {
-    return "It's a draw!";
+    result = "It's a draw!";
   } 
   // Conditions for paper
   else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beats Paper";
+    result = "You lose! Scissors beats Paper";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats Rock";
+    result = "You win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return "It's a draw!";
+    result = "It's a draw!";
   }
   // Conditions for Scissors
   else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock beats Scissors";
+    result = "You lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats Paper";
+    result = "You win! Scissors beats Paper";
   } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    return "It's a draw!";
+    result = "It's a draw!";
   } 
+  // Conditions for null
+  else {
+    result = "Please enter a value!";
+  }
+
+  console.log("Computer choice is " + computerSelection); // SEE COMPUTER SELECTION
+  
+  return result;
 }
 
-const playerSelection = "ScISSORS";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let playerPoint = 0;
+  let computerPoint = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let roundResult = playRound();
+    console.log(`Round ${i + 1}: ` + roundResult);
+    if (roundResult.slice(0,7) === "You win") {
+      playerPoint++;
+    }
+    else if (roundResult.slice(0,8) === "You lose") {
+      computerPoint++;
+    }
+    else if (roundResult === "It's a draw!") {
+      // do nothing
+    }
+    console.log(`The score so far is: PLAYER: ${playerPoint} ${isPoint(playerPoint)} COMPUTER: ${computerPoint} ${isPoint(computerPoint)}`);
+  }
+
+
+}
+
+function isPoint(point) {
+  if (point === 1) {
+    return "point";
+  } else {
+    return "points";
+  }
+}
+
+
+game();
 
 
   
